4a2abf0a821efd071616320d0e12651f2a72f726
Sarah Voshall <sarahvoshall@gmail.com>
2021-02-12 22:14:35 +0000

    2/12 ex 2

---
+++ easy_1/odd.rb
@@ -0,0 +1,14 @@
+def is_odd?(num)
+    if num % 2 != 0
+        num = true
+    else 
+        num = false
+    end 
+end 
+
+puts is_odd?(2)   
+puts is_odd?(5)    
+puts is_odd?(-17)
+puts is_odd?(-8)  
+puts is_odd?(0)    
+puts is_odd?(7)  
