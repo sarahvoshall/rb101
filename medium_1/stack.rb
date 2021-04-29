5501d5692b83d1673b2c10110904031c7c362923
Sarah Voshall <56939487+sarahvoshall@users.noreply.github.com>
2021-04-29 14:15:47 -0700

    4/29 again

---
+++ medium_1/stack.rb
@@ -0,0 +1,33 @@
+def minilang(string)
+  register = 0
+  stack = []
+  string.split.each do |command| 
+    case command 
+    when 'PUSH'   then stack << register
+    when 'ADD'    then register += stack.pop
+    when 'SUB'    then register -= stack.pop
+    when 'MULT'   then register *= stack.pop
+    when 'DIV'    then register /= stack.pop
+    when 'MOD'    then register %= stack.pop
+    when 'POP'    then register = stack.pop
+    when 'PRINT'  then puts register
+    else               register = command.to_i
+    end
+  end 
+end 
+
+minilang('PRINT')
+minilang('5 PUSH 3 MULT PRINT')
+minilang('5 PRINT PUSH 3 PRINT ADD PRINT')
+minilang('5 PUSH POP PRINT')
+minilang('3 PUSH 4 PUSH 5 PUSH PRINT ADD PRINT POP PRINT ADD PRINT')
+minilang('3 PUSH PUSH 7 DIV MULT PRINT ')
+minilang('4 PUSH PUSH 7 MOD MULT PRINT ')
+minilang('-3 PUSH 5 SUB PRINT')
+minilang('6 PUSH')
+
+
+
+
+
+
---
+++ medium_1/word_digit.rb
@@ -0,0 +1,30 @@
+DIGITS = {  'zero' => '0',
+            'one' => '1',
+            'two' => '2',
+            'three' => '3',
+            'four' => '4',
+            'five' => '5',
+            'six' => '6',
+            'seven' => '7',
+            'eight' => '8',
+            'nine' => '9'
+}
+
+def word_to_digit(string)
+  new_string = string.split.map do |word|
+    if DIGITS.has_key?(word)
+      word = DIGITS[word]
+    else 
+      word
+    end
+  end 
+  new_string.join(' ')
+end 
+
+# after reviewing LS solution and deciding that I should probably be 
+# more comfortable with using .gsub... 
+
+def word_to_digit2(string)
+  
+
+p word_to_digit('hello zero one two three')
